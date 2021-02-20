aa593ae2298670b22a5bbf752f6503c07e23b768
squibdev <adamjaredphillips@gmail.com>
2021-02-20 15:01:06 -0800

    Update 6kyu_tribonacci_sequence.py

--- python/6kyu_tribonacci_sequence.py
+++ python/6kyu_tribonacci_sequence.py
@@ -1,6 +1,5 @@
 def tribonacci(signature, n):
     a, b, c = signature
-    print(signature)
     if n <= 3:
         out = []
         for _ in range(n):
@@ -11,3 +10,5 @@ def tribonacci(signature, n):
             a, b, c = b, c, a+b+c
             signature.append(c)
     return signature
+
+print(tribonacci([0,1,1], 30))
